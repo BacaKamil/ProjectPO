23a3dc2c3d94cee9816f5b98aaa600db63879188
BacaKamil <kamil.baca@outlook.com>
2023-06-03 15:50:32 +0200

    Version 0.3.0

--- ProjectPO/LoginPanelScreen.xaml.cs
+++ ProjectPO/LoginPanelScreen.xaml.cs
@@ -1,27 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ProjectPO
 {
-    /// <summary>
-    /// Logika interakcji dla klasy LoginPanelScreen.xaml
-    /// </summary>
     public partial class LoginPanelScreen : Window
     {
         public LoginPanelScreen()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBoxLogin.Text;
+            string password = PasswordBoxPassword.Password;
+
+            using (SqlConnection connection = new SqlConnection("Server=LAPTOPKAMIL;Database=ProjectPO;Integrated Security=True;"))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT employeePassword FROM Employees WHERE employeeLogin = @Login";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Login", login);
+
+                        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(command.ExecuteScalar() as string);
+
+                        if (hashedPassword != null && BCrypt.Net.BCrypt.Verify(password, hashedPassword))
+                        {
+                            MainWindow mainWindow = new MainWindow();
+                            mainWindow.Show();
+                            Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid login or password. Please try again.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred during login: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
