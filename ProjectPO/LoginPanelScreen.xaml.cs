2ff40ce4e3ce9f1320b407e625f3f21c88b31814
BacaKamil <kamil.baca@outlook.com>
2023-06-04 15:09:28 +0200

    Version 0.3.2

--- ProjectPO/LoginPanelScreen.xaml.cs
+++ ProjectPO/LoginPanelScreen.xaml.cs
@@ -19,27 +19,34 @@ namespace ProjectPO
         {
             string login = TextBoxLogin.Text;
             string password = PasswordBoxPassword.Password;
-            EmployeeName = "NAME";
-            EmployeeLastName = "LASTNAME"; // branie z bazy name i lastname
-
 
             using (SqlConnection connection = new SqlConnection("Server=LAPTOPKAMIL;Database=ProjectPO;Integrated Security=True;"))
             {
                 try
                 {
                     connection.Open();
-                    string query = "SELECT employeePassword FROM Employees WHERE employeeLogin = @Login";
+                    string query = "SELECT employeeName, employeeLastName, employeePassword FROM Employees WHERE employeeLogin = @Login";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@Login", login);
+                        SqlDataReader reader = command.ExecuteReader();
 
-                        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(command.ExecuteScalar() as string);
-
-                        if (hashedPassword != null && BCrypt.Net.BCrypt.Verify(password, hashedPassword))
+                        if (reader.Read())
                         {
-                            MainWindow mainWindow = new MainWindow();
-                            mainWindow.Show();
-                            Close();
+                            EmployeeName = reader["employeeName"].ToString();
+                            EmployeeLastName = reader["employeeLastName"].ToString();
+                            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(reader["employeePassword"].ToString());
+
+                            if (hashedPassword != null && BCrypt.Net.BCrypt.Verify(password, hashedPassword))
+                            {
+                                MainWindow mainWindow = new MainWindow();
+                                mainWindow.Show();
+                                Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid login or password. Please try again.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
                         }
                         else
                         {
@@ -52,6 +59,7 @@ namespace ProjectPO
                     MessageBox.Show("An error occurred during login: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+
         }
     }
 }
